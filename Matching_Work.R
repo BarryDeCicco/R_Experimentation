22bc30fde2a276e662de354f4197b4cdfa7a290e
Barry DeCicco <bdecicco2001@yahoo.com>
2022-07-10 19:25:59 -0400

    Add files via upload
    This is the first file, working on merges.

---
+++ Matching_Work.R
@@ -0,0 +1,66 @@
+####    Load Libraries    ####
+
+
+library(magrittr)
+library(sqldf)
+library(tidyverse)
+
+####     Set up Data    ####
+Months <- month.name
+View(Months)
+Volume <-  c("a", "b", "c" ,"d" ,"e", "f", "g", "h", "i", "j" ,"k", "l")
+View(Volume)
+
+df1 <- as.data.frame(cbind(Months, Volume))
+View(df1)
+
+Case_Count <- c(runif(24,1,99))
+View(Case_Count)
+df2 <- as.data.frame(cbind(Months, Case_Count))
+View(df2)
+
+df2 <- df2 %>% 
+  dplyr::filter(!Months == "June")
+View(df2)
+
+
+####     Start Matching    ####
+
+
+####     Dplyr joins       ####
+
+  
+left_join_dplyr_df <- dplyr::left_join(df1, df2, by = "Months" )
+View(left_join_dplyr_df)
+
+
+
+right_join_dplyr_df <- dplyr::right_join(df1, df2, by = "Months" )
+View(right_join_dplyr_df)
+
+
+inner_join_dplyr_df <- dplyr::inner_join(df1, df2, by = "Months")
+View(inner_join_dplyr_df)
+
+
+full_join_dplyr_df <- dplyr::full_join(df1, df2, by = "Months")
+View(full_join_dplyr_df)
+
+####     sqldf joins       ####
+
+sql_left_join_df <- sqldf("select * from df1 a left join df2 b where a.Months = b.Months")
+View(sql_left_join_df)
+
+sql_full_join_df <- sqldf("select * from df1 a full outer join df2 b on a.Months = b.Months")
+View(sql_full_join_df)
+
+
+####     Base R merge joins       ####
+####     
+####     
+
+merge_all_x_df <- merge(df1, df2, by="Months", all.x = TRUE)
+View(merge_all_x_df)
+
+
+
